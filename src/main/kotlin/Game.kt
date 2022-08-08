bc7740b40d1598e1ee077a1363ef4043903c590d
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-08-08 19:41:23 +0300

    Game Status Play Again Feature Check The Board

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,3 +1,5 @@
+import kotlin.system.exitProcess
+
 class Game {
     private val board = MutableList<Cell>(size = 9) { Cell.Empty }
     private var status: Status = Status.Idle
@@ -70,7 +72,84 @@ class Game {
         }
     }
 
+    private fun checkTheBoard() {
+        val winningCombinations = listOf(
+            listOf(0, 1, 2),
+            listOf(3, 4, 5),
+            listOf(6, 7, 8),
+            listOf(0, 3, 6),
+            listOf(1, 4, 7),
+            listOf(2, 5, 8),
+            listOf(0, 4, 8),
+            listOf(2, 4, 6)
+        )
+        val player1Cells = mutableListOf<Int>()
+        val player2Cells = mutableListOf<Int>()
+        board.forEachIndexed { index, cell ->
+            if (cell.placeHolder == 'X') player1Cells.add(element = index)
+            if (cell.placeHolder == 'O') player2Cells.add(element = index)
+            println("YOUR MOVES: $player1Cells")
+            println("COMPUTER MOVES: $player2Cells")
 
+            run CombinationLoop@{
+                winningCombinations.forEach { combination ->
+                    if (player1Cells.containsAll(elements = combination)) {
+                        won()
+                        return@CombinationLoop
+                    }
+                    if (player2Cells.containsAll(elements = combination)) {
+                        lost()
+                        return@CombinationLoop
+                    }
+                }
+            }
+            if (board.none { it is Cell.Empty } && status is Status.Running) draw()
+            if (status is Status.GameOver) {
+                finish()
+                playAgain()
+            }
+
+        }
+    }
+
+    private fun finish() {
+        status = Status.Idle
+        board.replaceAll { Cell.Empty }
+    }
+
+    private fun playAgain() {
+        println("Do you wish to play another one? Y/N: ")
+        val input = readlnOrNull()
+        try {
+            require(value = input != null)
+            val capitalizedInput = input.replaceFirstChar(Char::titlecase)
+            val positive = capitalizedInput.contains(other = "Y")
+            val negative = capitalizedInput.contains(other = "N")
+            require(value = positive || negative)
+            if (positive) start() else if (negative) exitProcess(status = 0)
+        } catch (e: IllegalArgumentException) {
+            println("Wrong option. Type either 'Y' or 'N'")
+            playAgain()
+        }
+    }
+
+    private fun won() {
+        status = Status.GameOver
+        printBoard()
+        println("Congratulations ${player.name}, You WON!")
+    }
+
+    private fun lost() {
+        status = Status.GameOver
+        printBoard()
+        println("Sorry ${player.name}, You LOST!")
+    }
+
+    private fun draw() {
+        status = Status.GameOver
+        printBoard()
+        println("DRAW")
+    }
 
     private fun printBoard() {
         println()
